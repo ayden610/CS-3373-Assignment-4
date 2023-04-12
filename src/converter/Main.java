01b8f29c8310d041226845757f209bb13081e383
Ayden Duncan <aydenduncan610@gmail.com>
2023-04-11 20:15:02 -0400

    to Arbian

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -1,2 +1,19 @@
-package converter;public class Main {
+package converter;
+
+import converter.exceptions.MalformedNumberException;
+import converter.exceptions.ValueOutOfBoundsException;
+
+public class Main {
+
+
+    public Main() {
+
+    }
+
+    public static void main(String[] args) throws MalformedNumberException, ValueOutOfBoundsException {
+        String number = "I_X";
+        KibenianArabicConverter KAC = new KibenianArabicConverter(number);
+        KAC.toArabic();
+    }
+
 }
